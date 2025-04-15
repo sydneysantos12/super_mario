b22b14b8011a0044b46d7e50be2d297873a863e8
sydneysantos12 <samuelsantos.sud07@gmail.com>
2025-04-15 14:54:16 -0300

    final Version

--- super mario/js/script.js
+++ super mario/js/script.js
@@ -10,10 +10,20 @@ const jump = () => {
 
 const loop =setInterval(() => {
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = window.getComputedStyle(mario).bottom.replace('px','');
     
-    if (pipePosition <= 120){
+    if (pipePosition <= 120 && pipePosition >0 && marioPosition < 80){
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "img/game-over.png"
+        mario.style.width = '75px';
+        mario.style.marginleft = '50px'
+
+        clearInterval(loop);
     }
 
 }, 10);
